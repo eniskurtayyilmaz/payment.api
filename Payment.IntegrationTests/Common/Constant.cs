9be8115944b78d40619549f9de944760b07f02d9
Enis Kurtay YILMAZ <kurtay@codeapp.co>
2022-02-07 23:09:16 +0300

    IntegrationTests added. Tests done. All of cases done.

---
+++ Payment.IntegrationTests/Common/Constant.cs
@@ -0,0 +1,10 @@
+namespace Payment.IntegrationTests.Common
+{
+    public static class Constant
+    {
+        public static string PaymentLinkEndpoint = "/api/paymentLink";
+        
+        
+        public static string ContentType = "application/json";
+    }
+}
